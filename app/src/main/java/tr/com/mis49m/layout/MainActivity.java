bee4bb9050c12db133a9f0e01920741e7bc27f48
Ümit TOPAÇAN <UMITTOPACAN@kkb.com.tr>
2017-02-22 19:52:02 +0200

    Initial commit

---
+++ app/src/main/java/tr/com/mis49m/layout/MainActivity.java
@@ -0,0 +1,22 @@
+package tr.com.mis49m.layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gravity);
+
+    }
+}
